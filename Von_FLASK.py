3402e42c1e397be54448652e4f98a44550f920f5
jojo <jonathan.ndamba.pro@gmail.com>
2020-08-06 13:46:18 +0200

    Ajout dockerfile & requirements.txt

--- Von_FLASK.py
+++ Von_FLASK.py
@@ -1,7 +1,6 @@
 from flask import Flask, redirect, url_for, render_template
-import sqlite3
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def base():
@@ -12,7 +11,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
 
 
 
